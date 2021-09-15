3e55845aed3600d30ff3743a88aeb4638f1c3147
Niteshkumar-L <89631994+Niteshkumar-L@users.noreply.github.com>
2021-09-15 22:57:22 +0530

    Create Practice prg 8.java

---
+++ Practice prg 8.java
@@ -0,0 +1,11 @@
+class Cal1{ 
+void sum(int a,long b)
+{System.out.println("sum of  numbers:"+ (a+b));} 
+void sum(int a,int b,int c)
+{System.out.println("sum of three numbers:"+ (a+b+c));} 
+public static void main(String args[]){ 
+Cal1 c=new Cal1(); 
+c.sum(5,55);
+c.sum(9,8,6); 
+} 
+ } 
