7e1b43c5de7c25ecd6e8ef5f87fdfcb66573b5d9
Niteshkumar-L <89631994+Niteshkumar-L@users.noreply.github.com>
2021-09-16 22:22:46 +0530

    Create Practice prg 9.java

---
+++ Practice prg 9.java
@@ -0,0 +1,17 @@
+//purpose of default constructor
+class Stud1{ 
+int id; 
+String name; 
+
+void display()
+{
+  System.out.println(id+" "+name);
+  } 
+public static void main(String args[])
+{
+Stud1 s1=new Stud1(); 
+Stud1 s2=new Stud1(); 
+s1.display(); 
+s2.display(); 
+} 
+ }
