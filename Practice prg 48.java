3eeb949ba59b7abf5674cf8fe4bbe9b1fff57737
Niteshkumar-L <89631994+Niteshkumar-L@users.noreply.github.com>
2022-01-14 21:54:35 +0530

    Create Practice prg 48.java

---
+++ Practice prg 48.java
@@ -0,0 +1,14 @@
+public class PrintAsciiValueExample1   
+{  
+public static void main(String[] args)   
+{  
+
+char ch1 = 'c';  
+char ch2 = 'g';  
+
+int asciivalue1 = ch1;  
+int asciivalue2 = ch2;  
+System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
+System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
+}  
+}  
